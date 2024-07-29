14021e20e0ff65ed3b92782774f42ac597376491
Samuel Muigai <107753424+Ndungu-Muigai@users.noreply.github.com>
2024-07-29 11:32:54 +0300

    Added back the overflow hidden to the intro image slider

--- src/Pages/Shop/Home.js
+++ src/Pages/Shop/Home.js
@@ -38,7 +38,7 @@ const Shop = () =>
 
     return (
         <>
-            <div className="w-full p-1 mb-5 ">
+            <div className="w-full p-1 overflow-hidden mb-5 ">
                 <Slider {...settings}>
                     {
                         images.map((src, index) => 
