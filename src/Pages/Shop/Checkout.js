a630eba8d8969c76126638c0d9c538ba746a0389
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-08-04 00:51:24 +0300

    Added the counties, constituencies and wards to the checkout page

--- src/Pages/Shop/Checkout.js
+++ src/Pages/Shop/Checkout.js
@@ -1,10 +1,61 @@
-import Footer from "../../Components/Footer";
-import Logo from "../../Assets/Images/logo.png";
+/* eslint-disable react-hooks/exhaustive-deps */
+import Footer from "../../Components/Footer"
+import Logo from "../../Assets/Images/logo.png"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { KenyaAdministrativeDivisions } from 'kenya-administrative-divisions'
 
-import { Link } from "react-router-dom";
+const Checkout = () => {
+    // Initializing the Kenya Admin
+    const kenyaAdmin = new KenyaAdministrativeDivisions()
 
-const Checkout = () => 
-{
+    // useState hook for the form data
+    const [checkoutDetails, setCheckoutDetails] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        code: "+254",
+        county: "",
+        sub_county: "",
+        ward: "",
+        street: "",
+    })
+
+    // useState hooks for the counties, constituencies, and wards
+    const [counties, setCounties] = useState([])
+    const [constituencies, setConstituencies] = useState([])
+    const [wards, setWards] = useState([])
+
+    // Function to handle input change
+    const handleInputChange = e => setCheckoutDetails({ ...checkoutDetails, [e.target.name]: e.target.value })
+
+    // Getting everything from the Kenya Administrative Divisions
+    const data = kenyaAdmin.getAll()
+
+    // useEffect hook to set the fetched data
+    useEffect(() => 
+    {
+        setCounties(data)
+    }, [data])
+
+    // useEffect hook to update the constituencies based on the county
+    useEffect(() => 
+    {
+        const matchingConstituencies = counties.find(d => d.county_name === checkoutDetails.county)
+        setConstituencies(matchingConstituencies ? matchingConstituencies.constituencies : [])
+        setWards([])
+        setCheckoutDetails(prev => ({ ...prev, sub_county: "", ward: "" }))
+    }, [checkoutDetails.county, counties])
+
+    // useEffect hook to update the wards based on the sub county
+    useEffect(() => 
+    {
+        const matchingWards = constituencies.find(s => s.constituency_name === checkoutDetails.sub_county)
+        setWards(matchingWards ? matchingWards.wards : [])
+        setCheckoutDetails(prev => ({ ...prev, ward: "" }))
+    }, [checkoutDetails.sub_county, constituencies])
+
     return (
         <>
             <div className="flex items-center justify-center md:mx-8 p-4 md:text-lg">
@@ -23,56 +74,60 @@ const Checkout = () =>
                         <form className="w-full">
                             <div className="mb-4">
                                 <label className="block text-gray-700">Email Address <span className="span">*</span></label>
-                                <input type="email" name="email" className="input-field" placeholder="yourname@domain.com" required/>
+                                <input type="email" name="email" className="input-field" placeholder="yourname@domain.com" value={checkoutDetails.email} onChange={handleInputChange} required />
                             </div>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                                 <div>
                                     <label className="block text-gray-700">First Name <span className="span">*</span></label>
-                                    <input type="text" name="fname" className="input-field" placeholder="First Name" required/>
+                                    <input type="text" name="first_name" className="input-field" placeholder="First Name" value={checkoutDetails.first_name} onChange={handleInputChange} required />
                                 </div>
                                 <div>
                                     <label className="block text-gray-700">Last Name <span className="span">*</span></label>
-                                    <input type="text" name="lname" className="input-field" placeholder="Last Name" required/>
+                                    <input type="text" name="last_name" className="input-field" placeholder="Last Name" value={checkoutDetails.last_name} onChange={handleInputChange} required />
                                 </div>
                             </div>
                             <div className="mb-4">
                                 <label className="block text-gray-700">County <span className="span">*</span></label>
-                                <select className="input-field" name="county" required>
-                                    <option disabled selected>Select County</option>
-                                    <option>County A</option>
-                                    <option>County B</option>
+                                <select className="input-field" name="county" value={checkoutDetails.county} onChange={handleInputChange} required>
+                                    <option value="">Select County</option>
+                                    {
+                                        counties.map(county => 
+                                        <option key={county.county_code} value={county.county_name}>{county.county_name}</option>)
+                                    }
                                 </select>
                             </div>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                                 <div>
-                                    <label className="block text-gray-700">Sub County <span className="span">*</span></label>
-                                    <select className="input-field" name="sub-county" required>
-                                        <option disabled selected>Select Sub County</option>
-                                        <option>Sub County A</option>
-                                        <option>Sub County B</option>
+                                    <label className="block text-gray-700">Constituency <span className="span">*</span></label>
+                                    <select className="input-field" name="sub_county" value={checkoutDetails.sub_county} onChange={handleInputChange} disabled={!checkoutDetails.county} required>
+                                        <option value="">Select Constituency</option>
+                                        {
+                                            constituencies.map(s => <option key={s.constituency_name} value={s.constituency_name}>{s.constituency_name}</option>)
+                                        }
                                     </select>
                                 </div>
                                 <div>
                                     <label className="block text-gray-700">Ward <span className="span">*</span></label>
-                                    <select className="input-field" name="ward" required>
-                                        <option disabled selected>Select Ward</option>
-                                        <option>Ward A</option>
-                                        <option>Ward B</option>
+                                    <select className="input-field" name="ward" value={checkoutDetails.ward} onChange={handleInputChange} disabled={!checkoutDetails.sub_county} required>
+                                        <option value="">Select Ward</option>
+                                        {
+                                            wards.map(ward => <option key={ward} value={ward}>{ward}</option>)
+                                        }
                                     </select>
                                 </div>
                             </div>
                             <div className="mb-4">
                                 <label className="block text-gray-700">Street Address <span className="span">*</span></label>
-                                <input type="text" name="street" className="input-field" required/>
+                                <input type="text" name="street" className="input-field" value={checkoutDetails.street} onChange={handleInputChange} required />
                             </div>
                             <div className="flex flex-col md:flex-row gap-4 mb-4">
                                 <div className="w-1/4">
                                     <label className="block text-gray-700">Number Code</label>
-                                    <input type="text" name="code" value="+254" className="input-field" disabled required/>
+                                    <input type="text" name="code" className="input-field" value={checkoutDetails.code} disabled required />
                                 </div>
                                 <div className="w-full">
                                     <label className="block text-gray-700">Phone Number <span className="span">*</span></label>
-                                    <input type="text" name="phoneNumber" className="input-field" placeholder="7xxxxxxxx" required/>
+                                    <input type="text" name="phone" className="input-field" placeholder="7xxxxxxxx" required />
                                 </div>
                             </div>
                             <div className="flex justify-center mt-6">
@@ -87,9 +142,9 @@ const Checkout = () =>
                         <h2 className="font-bold text-xl uppercase text-center md:text-left">my order</h2>
                         <Link to="/cart" className="text-blue-600 hover:text-blue-800 font-semibold border border-blue-600 hover:border-blue-800 rounded px-3 py-1 transition-colors duration-200">Edit</Link>
                     </div>
-                    <hr className="mb-4"/>
+                    <hr className="mb-4" />
                     <div className="relative flex items-start p-2 mb-4">
-                        <img src={Logo} alt="Product name" className="h-24 w-24 object-cover rounded-lg"/>
+                        <img src={Logo} alt="Product name" className="h-24 w-24 object-cover rounded-lg" />
                         <div className="flex flex-col ml-4 flex-grow">
                             <h3 className="font-bold text-lg mb-2">Product name</h3>
                             <p>Qty: 1</p>
@@ -97,14 +152,14 @@ const Checkout = () =>
                         </div>
                     </div>
                     <div className="flex justify-between">
-                    	<p className="text-base mb-2">Subtotal</p>
+                        <p className="text-base mb-2">Subtotal</p>
                         <span className="font-semibold">KES 1,500</span>
                     </div>
                     <div className="flex justify-between">
                         <p className="text-base mb-2">Shipping</p>
                         <span className="font-semibold">KES 0</span>
                     </div>
-                    <hr className="m-3"/>
+                    <hr className="m-3" />
                     <div className="flex justify-between">
                         <p className="text-xl mb-4 font-bold">Total</p>
                         <span className="text-xl font-bold">KES 1,500</span>
@@ -113,7 +168,7 @@ const Checkout = () =>
             </div>
             <Footer />
         </>
-    );
-};
+    )
+}
 
-export default Checkout;
+export default Checkout
