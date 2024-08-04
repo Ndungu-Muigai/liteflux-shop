d9ad159d6d1af326d733417ddfd167226a149b91
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-08-04 19:32:47 +0300

    Fixed the update quantity issue in the cart page

--- src/Pages/Shop/Context/Cart Context.js
+++ src/Pages/Shop/Context/Cart Context.js
@@ -5,27 +5,34 @@ const CartContext = createContext()
 
 const saveCartToLocalStorage = cart => localStorage.setItem('cart', JSON.stringify(cart))
 
-const loadCartFromLocalStorage = () => {
+const loadCartFromLocalStorage = () => 
+{
     const savedCart = localStorage.getItem('cart')
     return savedCart ? JSON.parse(savedCart) : []
 }
 
-export const CartProvider = ({ children }) => {
+export const CartProvider = ({ children }) => 
+{
     const [cart, setCart] = useState(loadCartFromLocalStorage)
 
     useEffect(() => {
         saveCartToLocalStorage(cart)
     }, [cart])
 
-    const addToCart = (product, quantity) => {
-        setCart(prevCart => {
+    const addToCart = (product, quantity) => 
+    {
+        setCart(prevCart => 
+        {
             const existingProductIndex = prevCart.findIndex(item => item.product.id === product.id)
-            if (existingProductIndex !== -1) {
+            if (existingProductIndex !== -1) 
+            {
                 const newCart = [...prevCart]
                 newCart[existingProductIndex].quantity += quantity
                 toast.success(`Product quantity updated`)
                 return newCart
-            } else {
+            } 
+            else 
+            {
                 toast.success(`Product added to cart`)
                 return [...prevCart, { product, quantity }]
             }
