e0f520c49d346b483b5101d3161499733b17950d
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-08-08 09:13:26 +0300

    Styled the saved items component and added a confirmation modal for removing items from the wishlist

--- src/Pages/Shop/Saved Items.js
+++ src/Pages/Shop/Saved Items.js
@@ -4,8 +4,7 @@ import Footer from '../../Components/Footer'
 import { useCart } from './Context/Cart Context'
 import { useSavedItems } from './Context/Saved Items Context'
 
-const SavedItems = () => 
-{
+const SavedItems = () => {
   const { savedItems, removeItemFromSaved } = useSavedItems()
   const { addToCart } = useCart()
   const [isLoading, setIsLoading] = useState(true)
@@ -13,8 +12,7 @@ const SavedItems = () =>
   const [itemToRemove, setItemToRemove] = useState(null)
   const navigate = useNavigate()
 
-  useEffect(() => 
-  {
+  useEffect(() => {
     // Simulate fetching data with a timeout
     setTimeout(() => setIsLoading(false), 2000)
   }, [])
@@ -51,72 +49,65 @@ const SavedItems = () =>
 
   return (
     <>
-      <div className="p-4">
+      <div className="p-3">
         <h2 className="text-2xl font-bold mb-4">Saved Items</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-          {
-            isLoading 
-            ? 
-              (
-                // Placeholder cards while loading
-                Array.from({ length: 4 }).map((_, index) => (
-                  <div key={index} className="border p-4 rounded shadow animate-pulse max-w-xs">
-                    <div className="w-full h-48 bg-gray-200 mb-4 rounded"></div>
-                    <div className="h-6 bg-gray-200 mb-2"></div>
-                    <div className="h-4 bg-gray-200 mb-2"></div>
-                    <div className="h-4 bg-gray-200 mb-2"></div>
-                    <div className="flex flex-row mt-2 gap-4">
-                      <div className="w-24 h-10 bg-gray-200 rounded"></div>
-                      <div className="w-24 h-10 bg-gray-200 rounded"></div>
-                    </div>
-                    <div className="w-32 h-10 mt-5 bg-gray-200 rounded"></div>
-                  </div>
-                ))
-              ) 
-            : 
-              savedItems.length === 0 
-              ? (
-                  // Display when there are no saved items
-                  <div className="col-span-full text-center text-gray-500">
-                    No saved items found.
-                  </div>
-                ) 
-              : 
-                (
-                  // Display saved items
-                  savedItems.map((item) => 
-                  (
-                    <div key={item.id} className="border p-4 rounded shadow max-w-xs">
-                      <Link to={`/products/${item.name}`}>
-                        <img src={item.image} alt={item.name} className="w-full h-48 object-cover mb-4 rounded"/>
-                        <h3 className="text-xl font-semibold">{item.name}</h3>
-                        <p>{item.description}</p>
-                      </Link>
-                      <div className="flex flex-row justify-between mt-2 gap-4">
-                        <button onClick={() => handleBuyNow(item)} className="px-3 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">Buy Now</button>
-                        <button onClick={() => handleRemoveClick(item)} className="px-3 py-2 bg-red-500 text-white rounded hover:bg-red-600">Remove from wishlist</button>
-                      </div>
-                    </div>
-                  ))
-                )
-          }
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 w-full">
+          {isLoading ? (
+            // Placeholder cards while loading
+            Array.from({ length: 4 }).map((_, index) => (
+              <div key={index} className="border p-4 rounded shadow animate-pulse max-w-xs">
+                <div className="w-full h-48 bg-gray-200 mb-4 rounded"></div>
+                <div className="h-6 bg-gray-200 mb-2"></div>
+                <div className="h-4 bg-gray-200 mb-2"></div>
+                <div className="h-4 bg-gray-200 mb-2"></div>
+                <div className="flex flex-row mt-2 gap-4">
+                  <div className="w-24 h-10 bg-gray-200 rounded"></div>
+                  <div className="w-24 h-10 bg-gray-200 rounded"></div>
+                </div>
+                <div className="w-32 h-10 mt-5 bg-gray-200 rounded"></div>
+              </div>
+            ))
+          ) : savedItems.length === 0 ? (
+            // Display when there are no saved items
+            <div className="col-span-full text-center text-gray-500">
+              No saved items found.
+            </div>
+          ) : (
+            // Display saved items
+            savedItems.map((item) => 
+            (
+              <div key={item.id} className="border p-3 rounded shadow w-full flex flex-col">
+                <Link to={`/products/${item.name}`} className="flex-grow">
+                  <img src={item.image} alt={item.name} className="w-full h-52 object-cover mb-4 rounded" />
+                  <h3 className="text-xl font-semibold">{item.name}</h3>
+                  <p className='text-2xl mb-2'>{item.description}</p>
+                </Link>
+                <div className="flex flex-row justify-between mt-3 gap-4">
+                  <button onClick={() => handleBuyNow(item)} className="px-2 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">Buy Now</button>
+                  <button onClick={() => handleRemoveClick(item)} className="px-2 py-2 bg-red-500 text-white rounded hover:bg-red-600">Remove from wishlist</button>
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
 
       {
         isModalOpen && 
-        (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded shadow-md max-w-sm w-full">
-              <h3 className="text-xl font-semibold mb-4">Confirm Removal</h3>
-              <p>Are you sure you want to remove <strong>{itemToRemove?.name}</strong> from your wishlist?</p>
-              <div className="flex justify-end mt-4 gap-2">
-                <button onClick={handleCancelRemove} className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Cancel</button>
-                <button onClick={handleConfirmRemove} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">Confirm</button>
+          (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-white p-6 rounded shadow-md max-w-sm w-full">
+                <h3 className="text-xl font-semibold mb-4">Confirm Removal</h3>
+                <p>
+                  Are you sure you want to remove <strong>{itemToRemove?.name}</strong> from your wishlist?
+                </p>
+                <div className="flex justify-end mt-4 gap-2">
+                  <button onClick={handleCancelRemove} className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Cancel</button>
+                  <button onClick={handleConfirmRemove} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">Confirm</button>
+                </div>
               </div>
             </div>
-          </div>
-        )
+          )
       }
 
       <Footer />
